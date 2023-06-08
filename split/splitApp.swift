0fbba1670471816b5b4409d80a4dabe9dbc04a1f
Konrad <kfheyen@gmail.com>
2023-06-08 00:06:12 -0400

    Initial Commit

---
+++ split/splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitApp.swift
+//  split
+//
+//  Created by Konrad Heyen on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ splitUITests/splitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  splitUITestsLaunchTests.swift
+//  splitUITests
+//
+//  Created by Konrad Heyen on 6/8/23.
+//
+
+import XCTest
+
+final class splitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
